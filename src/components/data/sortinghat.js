bd4f044627a85186820e2d3de440fc12bb7a4dab
nishaya raj <nishayaraj@gmail.com>
2022-05-19 15:52:23 -0500

    modularizing

---
+++ src/components/data/sortinghat.js
@@ -0,0 +1,28 @@
+import studentsOnDom from '../../scripts/components/cards';
+import houses from './houses';
+import students from './studentData';
+
+// ********** LOGIC  ********** //
+// sorts student to a house and then place them in the students array
+const sortStudent = (e) => {
+  e.preventDefault();
+  const sortingHat = houses[Math.floor(Math.random() * houses.length)];
+
+  if (e.target.id === 'sorting') {
+    const student = document.querySelector('#student-name');
+
+    // create the new student object
+    students.push({
+      // id: createId(students),
+      name: student.value,
+      house: sortingHat.house,
+      crest: sortingHat.crest
+    });
+
+    student.value = ''; // reset value of input
+    studentsOnDom('#students', students);
+    document.querySelector('#sorting').addEventListener('submit', sortStudent);
+  }
+};
+
+export default sortStudent;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,97 @@
-// USE WITH FIREBASE AUTH
-// import checkLoginStatus from './helpers/auth';
-import 'bootstrap'; // import bootstrap elements and js
-import '../styles/main.scss';
+import renderToDOM from './helpers/renderToDom';
+import sortStudent from '../components/data/sortinghat';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
+// ********** HTML Components  ********** //
+// the basic HMTL structure of app
+const htmlStructure = () => {
+  const domString = `
+  <div id="header-container" class="header mb-3"></div>
+  <div id="form-container" class="container mb-3 text-center"></div>
+  <div id="filter-container" class="container mb-3"></div>
+  <div id="student-container" class="container d-flex"></div>
   `;
-  console.warn('YOU ARE UP AND RUNNING!');
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+  renderToDOM('#app', domString);
 };
 
-init();
+const header = () => {
+  const domString = `<div class="container">
+    <h1>Welcome to Hoggy Hogwarts Sorting Hat!</h1>
+    <p>
+      Hmm, difficult. VERY difficult. <br />Plenty of courage, I see.
+      <br />Not a bad mind, either. There's talent, oh yes. <br />And a
+      thirst to prove yourself. <br />But where to put you?
+    </p>
+  </div>`;
+
+  renderToDOM('#header-container', domString);
+};
+
+const startSortingBtn = () => {
+  const domString = '<button type=\'button\' class=\'btn btn-info\' id=\'start-sorting\'>Start the Sorting Ceremony!</button>';
+
+  renderToDOM('#form-container', domString);
+};
+
+const studentAreas = () => {
+  const domString = `<div id="students">No Students</div>
+  <div id="voldy">No Death Eaters</div>`;
+
+  renderToDOM('#student-container', domString);
+};
+
+const filterBtnRow = () => {
+  const domString = `<div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" id="filter--hufflepuff" class="btn btn-warning btn-sm">Hufflepuff</button>
+      <button type="button" class="btn btn-primary btn-sm" id="filter--ravenclaw">Ravenclaw</button>
+      <button type="button" class="btn btn-success btn-sm" id="filter--slytherin">Slytherin</button>
+      <button type="button" class="btn btn-danger btn-sm" id="filter--gryffindor">Gryffindor</button>
+      <button type="button" class="btn btn-secondary btn-sm" id="filter--all">All</button>
+    </div>`;
+
+  renderToDOM('#filter-container', domString);
+};
+
+// Create a new ID for the students
+const createId = (array) => {
+  if (array.length) {
+    const idArray = array.map((el) => el.id);
+    return Math.max(...idArray) + 1;
+  }
+  return 0;
+};
+
+// add form to DOM on start-sorting click.
+// Add events for form after the form is on the DOM
+const form = () => {
+  const domString = `<form id="sorting" class="d-flex flex-column form-floating ">
+    <input
+    type="text"
+    class="form-control mb-1"
+    id="student-name"
+    placeholder="Enter a name"
+    required
+  />
+  <label for="floatingInputValue">Name to be sorted</label>
+<button type="submit" class="btn btn-success">Get Sorted!</button>
+</form>`;
+
+  renderToDOM('#form-container', domString);
+
+  // has to be put on the DOM after form is on DOM, not before
+  // on form submit, sort student
+  document.querySelector('#sorting').addEventListener('submit', sortStudent);
+};
+
+const startApp = () => {
+  htmlStructure(); // always load first
+  header();
+  form(); // form
+  startSortingBtn();
+  filterBtnRow(); // filter buttons
+  studentAreas(); // students and voldy's army divs
+};
+
+startApp();
+
+export default createId;
